61b4c880181ca1800e2069a15907cc1d665209b5
yinm <yinm.cminor9@gmail.com>
2017-08-14 00:55:29 +0900

    extract method 2

--- ch2/app.js
+++ ch2/app.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const fs = require('fs');
+let request;
+let response;
 
 const server = http.createServer(getFromClient);
 
@@ -8,11 +10,14 @@ console.log('Server start!');
 
 // createServerの処理
 function getFromClient(req, res) {
-  const request = req;
-  const response = res;
-  fs.readFile('./index.html', 'utf-8', (error, data) => {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write(data);
-    response.end();
-  });
+  request = req;
+  response = res;
+  fs.readFile('./index.html', 'utf-8', writeToResponse);
+}
+
+// readFile完了後の処理
+function writeToResponse(error, data) {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write(data);
+  response.end();
 }
