4286f575cc4a736d5e2b720e577c3b13401d3d9e
yinm <yinm.cminor9@gmail.com>
2017-08-14 01:24:05 +0900

    add rendering ejs

--- ch2/app.js
+++ ch2/app.js
@@ -1,20 +1,17 @@
 const http = require('http');
 const fs = require('fs');
+const ejs = require('ejs');
+
+const indexPage = fs.readFileSync('./index.ejs', 'utf8');
 
 const server = http.createServer(getFromClient);
 
 server.listen(3000);
 console.log('Server start!');
 
-// createServerの処理
 function getFromClient(req, res) {
-  fs.readFile('./index.html', 'utf-8', (error, data) => {
-    const content = data
-      .replace(/dummy_title/g, 'タイトルです')
-      .replace(/dummy_content/g, 'これがコンテンツです。');
-
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(content);
-    res.end();
-  });
+  const content = ejs.render(indexPage);
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(content);
+  res.end();
 }
