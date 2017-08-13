d303a84773a9dce5574556b49b334f57ea90e532
yinm <yinm.cminor9@gmail.com>
2017-08-14 00:40:44 +0900

    render index.html

--- ch2/app.js
+++ ch2/app.js
@@ -1,18 +1,13 @@
 const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer(
-  (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<!doctype html><html lang="ja">');
-    res.write('<head><meta charset="utf-8">');
-    res.write('<title>Hello</title></head>');
-    res.write('<body><h1>Hello Node.js!</h1>');
-    res.write('<p>This is Node.js sample page.</p>');
-    res.write('<p>これは、Node.jsのサンプルページです。</p>', 'utf8');
-    res.write('</body></html>');
+const server = http.createServer((req, res) => {
+  fs.readFile('./index.html', 'utf-8', (error, data) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
     res.end();
-  }
-);
+  });
+});
 
 server.listen(3000);
 console.log('Server start!');
