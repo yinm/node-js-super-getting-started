a22e5a432abbe8bfac944a93184632f8972ac8c9
yinm <yinm.cminor9@gmail.com>
2017-08-14 00:26:34 +0900

    add header

--- ch2/app.js
+++ ch2/app.js
@@ -2,8 +2,17 @@ const http = require('http');
 
 const server = http.createServer(
   (req, res) => {
-    res.end('<html><body><h1>Hello</h1><p>Welcome to Node.js</p></body></html>');
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<!doctype html><html lang="ja">');
+    res.write('<head><meta charset="utf-8">');
+    res.write('<title>Hello</title></head>');
+    res.write('<body><h1>Hello Node.js!</h1>');
+    res.write('<p>This is Node.js sample page.</p>');
+    res.write('<p>これは、Node.jsのサンプルページです。</p>', 'utf8');
+    res.write('</body></html>');
+    res.end();
   }
 );
 
 server.listen(3000);
+console.log('Server start!');
