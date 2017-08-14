1701e96a5016fa7a67a51192385195d7f38986b8
yinm <yinm.cminor9@gmail.com>
2017-08-15 01:57:27 +0900

    add pagination

--- ch6/ex-gen-app/routes/hello.js
+++ ch6/ex-gen-app/routes/hello.js
@@ -190,4 +190,27 @@ router.post('/find', (req, res, next) => {
     });
 });
 
+Bookshelf.plugin('pagination');
+
+router.get('/:page', (req, res, next) => {
+  let pg = req.params.page;
+  pg *= 1;
+  if (pg < 1) { pg = 1 }
+
+  new MyData().fetchPage({page: pg, pageSize: 3}).then((collection) => {
+    const data = {
+      title: 'Hello!',
+      content: collection.toArray(),
+      pagination: collection.pagination
+    };
+
+    console.log(collection.pagination);
+    res.render('hello/index', data);
+
+  })
+  .catch((err) => {
+    res.status(500).json({error: true, data: {message: err.message}});
+  });
+});
+
 module.exports = router;
