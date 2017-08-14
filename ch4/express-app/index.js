d66731c800157ed46c0ccafb0160e7cf7b253b2c
yinm <yinm.cminor9@gmail.com>
2017-08-14 14:46:40 +0900

    add other page

--- ch4/express-app/index.js
+++ ch4/express-app/index.js
@@ -2,18 +2,36 @@ const express = require('express');
 const ejs = require('ejs');
 
 const app = express();
-
 app.engine('ejs', ejs.renderFile);
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   const msg =
-    'This is Express Page!<br>'
-  + 'これは、スタイルシートを利用した例です。';
+    'This is Index Page!<br>'
+  + 'これは、トップページです。';
 
   res.render('index.ejs', {
     title: 'Index',
     content: msg,
+    link: {
+      href: '/other',
+      text: '別のページに移動',
+    },
+  });
+});
+
+app.get('/other', (req, res) => {
+  const msg =
+    'This is Other Page!<br>'
+  + 'これは、用意された別のページです。';
+
+  res.render('index.ejs', {
+    title: 'other',
+    content: msg,
+    link: {
+      href: '/',
+      text: 'トップに戻る',
+    },
   });
 });
 
