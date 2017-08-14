7bf59dfd51926d2da7bd22335b83e3df27939028
yinm <yinm.cminor9@gmail.com>
2017-08-14 19:16:12 +0900

    add form submit function

--- ch5/ex-gen-app/routes/hello.js
+++ ch5/ex-gen-app/routes/hello.js
@@ -2,12 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  const name = req.query.name;
-  const mail = req.query.mail;
-
   const data = {
     title: 'Hello!',
-    content: 'あなたの名前は、' + name + '。<br>メールアドレスは、' + mail + 'です。',
+    content: '何か書いて送信してください。'
+  };
+
+  res.render('hello', data);
+});
+
+router.post('/post', (req, res, next) => {
+  const msg = req.body['message'];
+  const data = {
+    title: 'Hello!',
+    content: 'あなたは、「' + msg + '」と送信しました。'
   };
 
   res.render('hello', data);
