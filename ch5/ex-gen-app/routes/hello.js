8d125f4f0d8c671eff81cd3eaa5b70c6e4f59c26
yinm <yinm.cminor9@gmail.com>
2017-08-14 19:10:26 +0900

    add getting query string

--- ch5/ex-gen-app/routes/hello.js
+++ ch5/ex-gen-app/routes/hello.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
+  const name = req.query.name;
+  const mail = req.query.mail;
+
   const data = {
     title: 'Hello!',
-    content: 'これは、サンプルのコンテンツです。<br>this is sample content.'
+    content: 'あなたの名前は、' + name + '。<br>メールアドレスは、' + mail + 'です。',
   };
 
   res.render('hello', data);
