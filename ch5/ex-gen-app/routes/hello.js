f995086d913caf2f70f73f2e496b4506b15fbf06
yinm <yinm.cminor9@gmail.com>
2017-08-14 23:39:51 +0900

    add getting data from sqlite3

--- ch5/ex-gen-app/routes/hello.js
+++ ch5/ex-gen-app/routes/hello.js
@@ -1,34 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const sqlite3 = require('sqlite3');
 
-const https = require('https');
-const parseString = require('xml2js').parseString;
+const db = new sqlite3.Database('mydb.sqlite3');
 
 router.get('/', (req, res, next) => {
-  const opt = {
-    host: 'news.google.com',
-    port: 443,
-    path: '/news?hl=ja&ned=us&ie=UTF-8&oe=UTF-8&output=rss'
-  };
-
-  https.get(opt, (res2) => {
-    let body = '';
-
-    res2.on('data', (data) => {
-      body += data;
-    });
-
-    res2.on('end', () => {
-      parseString(body.trim(), (err, result) => {
+  db.serialize(() => {
+    db.all('select * from mydata', (err, rows) => {
+      if (!err) {
         const data = {
           title: 'Hello!',
-          content: result.rss.channel[0].item
+          content: rows
         };
 
         res.render('hello', data);
-      });
+      }
     });
   });
+
 });
 
 module.exports = router;
