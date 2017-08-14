129c0d752b2607a8ae00862867ddbab8d264eb97
yinm <yinm.cminor9@gmail.com>
2017-08-14 14:28:38 +0900

    apply stylesheet

--- ch4/express-app/index.js
+++ ch4/express-app/index.js
@@ -4,14 +4,19 @@ const ejs = require('ejs');
 const app = express();
 
 app.engine('ejs', ejs.renderFile);
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
+  const msg =
+    'This is Express Page!<br>'
+  + 'これは、スタイルシートを利用した例です。';
+
   res.render('index.ejs', {
     title: 'Index',
-    content: 'This is Express-app Top page!'
+    content: msg,
   });
 });
 
-const server = app.listen(3000, () => {
+app.listen(3000, () => {
   console.log('Server is running!');
 });
