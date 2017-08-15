634920d45b83a217a5bc2e6ed42500bee5588556
yinm <yinm.cminor9@gmail.com>
2017-08-15 12:20:17 +0900

    add index script

--- ch7/mini_board_2/routes/index.js
+++ ch7/mini_board_2/routes/index.js
@@ -1,9 +1,81 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const knex = require('knex')({
+  dialect: 'sqlite3',
+  connection: {
+    filename: 'board_data.sqlite3'
+  },
+  useNullAsDefault: true
+});
+
+const Bookshelf = require('bookshelf')(knex);
+
+Bookshelf.plugin('pagination');
+
+const User = Bookshelf.Model.extend({
+  tableName: 'users'
+});
+
+const Message = Bookshelf.Model.extend({
+  tableName: 'messages',
+  hasTimestamps: true,
+  user: function() {
+    return this.belongsTo(User);
+  }
+});
+
+router.get('/', (req, res, next) => {
+  if (req.session.login === null) {
+    res.redirect('/users');
+  } else {
+    res.redirect('/1');
+  }
+});
+
+router.get('/:page', (req, res, next) => {
+  if (req.session.login === null) {
+    res.redirect('/users');
+    return;
+  }
+
+  let page = req.params.page;
+  page *= 1;
+  if (page < 1) {
+    page = 1;
+  }
+
+  new Message()
+    .orderBy('created_at', 'DESC')
+    .fetchPage({page:page, pageSize:10, withRelated: ['user']})
+    .then((collection) => {
+      const data = {
+        title: 'miniBoard',
+        login: req.session.login,
+        collection: collection.toArray(),
+        pagination: collection.pagination
+      };
+
+      res.render('index', data);
+    }).catch((err) => {
+      res.status(500).json({
+        error: true,
+        data: {
+          message: err.message
+        }
+      });
+  });
+});
+
+router.post('/', (req, res, next) => {
+  const record = {
+    message: req.body.msg,
+    user_id: req.session.login.id
+  }
+
+  new Message(record).save().then((model) => {
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
